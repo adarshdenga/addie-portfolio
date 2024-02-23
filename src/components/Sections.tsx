d82176077648f1ab144d0ac74659eff6625c06c8
mynameadarsh <adarshdenga@gmail.com>
2024-02-23 20:26:36 +0100

    Removed unused dependencies, touched up mobile stuff

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -22,14 +22,14 @@ export default function Sections() {
   return (
     <div className="flex flex-row md:flex-col lg:flex-col text-gray text-lg md:text-5xl lg:text-8xl font-barlow justify-center md:justify-normal lg:justify-normal">
       {sections.map((section, index) => (
-        <div
-          className={selectedIndex === index ? "text-white mr-2" : "mr-2"}
+        <p
+          className={selectedIndex === index ? "text-white mr-2 " : "mr-2"}
           onClick={() => {
             setSelectedIndex(index);
           }}
         >
           {section.name}
-        </div>
+        </p>
       ))}
     </div>
   );
