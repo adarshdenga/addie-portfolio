3f227adcdc82d7b05414948482e1fda780b785f1
mynameadarsh <adarshdenga@gmail.com>
2024-03-01 07:19:26 +0100

    Automated the max drag scroll distance

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,40 +1,26 @@
 export default function Skills() {
-  //   const skills = [
-  //     "Java",
-  //     "Python",
-  //     "TensorFlow",
-  //     "PyTorch",
-  //     "React",
-  //     "TypeScript",
-  //     "JavaScript",
-  //     "TailwindCSS",
-  //     "HTML5",
-  //     "CSS3",
-  //     "Bootstrap",
-  //     "Git",
-  //   ];
 
   const skills = [
-    { name: "Java", source: "src/icons/java.png" },
-    { name: "Python", source: "src/icons/python.png" },
-    { name: "TensorFlow", source: "src/icons/tensorflow.png" },
-    { name: "PyTorch", source: "src/icons/pytorch.png" },
-    { name: "React", source: "src/icons/react.png" },
-    { name: "TypeScript", source: "src/icons/typescript.png" },
-    { name: "JavaScript", source: "src/icons/javascript.png" },
-    { name: "TailwindCSS", source: "src/icons/tailwindcss.png" },
-    { name: "HTML5", source: "src/icons/html5.png" },
-    { name: "CSS3", source: "src/icons/css3.png" },
-    { name: "Bootstrap", source: "src/icons/bootstrap.png" },
-    { name: "Git", source: "src/icons/git.png" },
+    { name: "Java", source: "src/assets/icons/java.png" },
+    { name: "Python", source: "src/assets/icons/python.png" },
+    { name: "TensorFlow", source: "src/assets/icons/tensorflow.png" },
+    { name: "PyTorch", source: "src/assets/icons/pytorch.png" },
+    { name: "React", source: "src/assets/icons/react.png" },
+    { name: "TypeScript", source: "src/assets/icons/typescript.png" },
+    { name: "JavaScript", source: "src/assets/icons/javascript.png" },
+    { name: "TailwindCSS", source: "src/assets/icons/tailwindcss.png" },
+    { name: "HTML5", source: "src/assets/icons/html5.png" },
+    { name: "CSS3", source: "src/assets/icons/css3.png" },
+    { name: "Bootstrap", source: "src/assets/icons/bootstrap.png" },
+    { name: "Git", source: "src/assets/icons/git.png" },
   ];
 
   return (
-    <div className="flex flex-wrap justify-end content-start">
+    <div className="flex flex-wrap justify-end content-start gap-2">
       {skills.map((skill) => (
-        <div className="flex flex-row justify-center p-2 m-1 bg-gray rounded-full">
+        <div className="flex flex-row justify-center gap-2 p-2 bg-gray rounded-full font-inter text-sm">
           <img src={skill.source} className="h-5 w-5"></img>
-          <div className="px-2 text-sm">{skill.name}</div>
+          <div>{skill.name}</div>
         </div>
       ))}
     </div>
